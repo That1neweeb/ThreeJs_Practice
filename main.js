9aa4f2543c98658a7a4942893a8fccaa66396d54
That1neweeb <sidhant.giri32@gmail.cpm>
2026-04-01 08:27:53 +0545

    Day3 : Animating and creating instances through function

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 import * as THREE from 'three';
-import { rotate } from 'three/tsl';
+import { color } from 'three/tsl';
+// import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
@@ -8,17 +9,40 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 
 document.body.appendChild( renderer.domElement );
-
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
-
 camera.position.z = 5;
+const texLoader = new THREE.TextureLoader();
+const texture1 = texLoader.load('public/textures/texture1.png');
+
+function makeInstance(geometry, color, x){
+
+  const material = new THREE.MeshPhongMaterial({color});
+
+  const cube = new THREE.Mesh(geometry,material);
+  scene.add(cube);
+  cube.position.x = x;
+
+  return cube; 
+}
+
+const geometry = new THREE.BoxGeometry(1,1,1);
+const cubes = [
+  makeInstance(geometry , 0x44aa88, 0),
+  makeInstance(geometry, 0x44aa88,1),
+];
+
+const material = new THREE.MeshPhongMaterial( { map: texture1 });
+const geometry2 = new THREE.SphereGeometry(0.5, 32, 52);
+const sphere = new THREE.Mesh( geometry2 , material );
+scene.add (sphere); 
+sphere.position.x = 2
+
+const hemiLight = new THREE.DirectionalLight(0xFFFFFF,3);
+hemiLight.position.set(-1,2,4);
+scene.add(hemiLight);
 
 function animate( time ) {
-  cube.rotation.x = time/2000;
-  cube.rotation.y = time/1000;
+  // cube.rotation.x = time/2000;
+  // cube.rotation.y = time/1000;
   renderer.render( scene, camera );
 
 }
